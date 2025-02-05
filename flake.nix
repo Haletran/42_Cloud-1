08b5a9e9b0e83cd1d587fe7e70082587e73f2c56
Haletran <bpasquier123@gmail.com>
2025-02-05 15:59:29 +0100

    fix: flake.nix so that ansible work

--- flake.nix
+++ flake.nix
@@ -5,9 +5,14 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.python3
     pkgs.ansible
+	pkgs.sshpass 
+	pkgs.glibcLocales
   ];
 
   shellHook = ''
+	#export LC_ALL=en_US.UTF-8
+	#export LANG=en_US.UTF-8
+
     echo "Ansible environment is ready!"
     ansible --version
   '';
